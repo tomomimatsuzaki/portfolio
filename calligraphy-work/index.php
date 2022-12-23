11a22b8fd2c8193c429fe07ab4824654471b1e91
Tomomi <115207510+tomomimatsuzaki@users.noreply.github.com>
2022-12-23 13:10:05 -0800

    Add files via upload

---
+++ calligraphy-work/index.php
@@ -0,0 +1,83 @@
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/header.php";
+   include_once($path);
+?> 
+
+
+<section class="project-box">
+<div class="project-heading">
+   
+    <div class="step-left">
+        
+        <h1 class="project-title">Calligraphy Work</h1>
+        <p>#Artwork #PersonalWork</p>
+        
+    </div>
+    
+    <div class="step-right">
+        <div class="title-box">
+                <h3 class="small-title">Overview</h3> 
+        </div>
+            <p>This page is to show my artwork, calligraphy. I grabbed my first pen in 2018 and immediately fell in love with its beautiful forms and its relaxing and calming effects. I am gradually expanding my calligraphy community through social media and enjoying designing for events decorations, celebration cards, and envelopes. My goal is to bring people a sense of joy and happiness through my calligraphy work. </p>
+           
+    </div>
+</div>
+    
+<div class="project-center">
+        <div class="center-content calligraphy-container">
+            <img src="/images/calligraphy1.jpeg" alt="Calligraphy Work" width="40%" class="calligraphy-image"> 
+            <img src="/images/calligraphy7.JPG" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            <img src="/images/calligraphy9.jpg" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            <img src="/images/calligraphy10.JPG" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            
+            <img src="/images/calligraphy13.jpg" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            <img src="/images/calligraphy6.JPG" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            <img src="/images/calligraphy2.png" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            <img src="/images/calligraphy14.JPG" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            <img src="/images/calligraphy8.JPG" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            <img src="/images/calligraphy4.jpeg" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            
+            <img src="/images/calligraphy5.jpeg" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            <img src="/images/calligraphy3.jpeg" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            <img src="/images/calligraphy11.JPG" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            <img src="/images/calligraphy16.JPG" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            <img src="/images/calligraphy15.JPG" alt="Calligraphy Work" width="40%" class="calligraphy-image">
+            
+        </div>
+    </div>
+    
+    <br>
+    
+    
+    
+   
+    
+ 
+ 
+    
+    <div class="btn-container">
+    <section class="color-1">
+        <p>
+            <a href="/graphic-design/"><button class="btn">&larr; Back</button></a>
+            <a href="/safe-haven/"><button class="btn">Next &rarr;</button></a>
+        </p>
+    </section>
+    </div>
+</section>
+
+   
+<!-- Footer -->
+<footer>
+    <small>©2021 Website Desinged & Coded by Tomomi Matsuzaki</small>
+</footer>    
+
+    
+    
+    
+
+     
+</body>
+</html>
+
+ 
